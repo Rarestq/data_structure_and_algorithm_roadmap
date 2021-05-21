a366956ce0b3af1290719231c24e153577e40706
无朽 <wuxiu@maihaoche.com>
2021-05-21 14:34:52 +0800

    quick sort optimization solution

--- src/main/java/com/rarestzhou/leetcode_solutions/java/sorting/QuickSortSolutions.java
+++ src/main/java/com/rarestzhou/leetcode_solutions/java/sorting/QuickSortSolutions.java
@@ -66,10 +66,69 @@ public class QuickSortSolutions {
             return;
         }
         // 分区，并返回基准值位置
-        int pivot = partition(arr, left, right);
+//        int pivot = partition(arr, left, right);
         // 递归对基准值左右两个区间进行排序
-        quickPartitionSort(arr, left, pivot - 1);
-        quickPartitionSort(arr, pivot + 1, right);
+//        quickPartitionSort(arr, left, pivot - 1);
+//        quickPartitionSort(arr, pivot + 1, right);
+
+        // 快排优化一：单边递归优化
+        singleSideQuickPartitionSort(arr, left, right);
+    }
+
+    /**
+     * 快排优化一：单边递归优化（对程序的实际运行时间做的优化）
+     *
+     * partition 分区后，在对基准值左右两边进行递归调用排序的时候，可以尝试减少调用此时
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void singleSideQuickPartitionSort(int[] arr, int left, int right) {
+        while (left < right) {
+            // 进行一轮 partition 操作
+            // 获得基准值的位置
+            int ind = partition(arr, left, right);
+            // 右侧正常调用递归函数
+            singleSideQuickPartitionSort(arr, ind  +1, right);
+            // 用本层处理左侧的排序
+            right = ind - 1;
+        }
+    }
+
+    /**
+     * 快排优化二：基准值选取优化（三点取中法）--> 使快速排序的时间复杂度尽量稳定在O(N*logN)二提出来的
+     *
+     * 只有当基准值每次都能(尽可能)将排序区间中的数据平分时，时间复杂度才是最好情况下的 O(N*logN)，
+     * 不然就可能退化为和选择排序、插入排序一样的时间复杂度O(N^2)
+     *
+     * 三点取中法：每一轮取排序区间的头、尾和中间元素这三个值，然后把它们排序以后的中间值作为本轮的基准值
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void selectPivot(int[] arr, int left, int right) {
+
+    }
+
+    /**
+     * 快排优化三：partition 操作优化（对程序的实际运行时间做的优化）
+     *
+     * partition 的实现过程：先从后向前找个小于基准值的数字放到前面，再从前向后找个大于基准值的数字放到后面，直到首尾指针相遇为止
+     *
+     * 基准值的客观位置不变，红色与绿色元素数量是确定的，所以存在多少个绿色元素在基准值位置的后面，就一定存在多少个红色元素在基准值位置的前面
+     *
+     * 那 partition 操作的目的，就是要把基准值位置后面的绿色元素调整到前面，将基准值位置前面的红色元素调整到后面。
+     * 既然需要调换的红色与绿色元素的数量相同，我们就可以让头指针向后查找红色元素，尾指针向前查找绿色元素，然后交换头尾指针所指向的元素，
+     * 重复这个过程，直到头尾指针交错后停止。这就是对 partition 操作进行的优化
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void partitionOptimize(int[] arr, int left, int right) {
+
     }
 
     /**
